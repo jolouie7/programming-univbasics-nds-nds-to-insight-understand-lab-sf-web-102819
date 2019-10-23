b6f466f9dc231f4e6a4f31d08a978cd58b318302
Joseph  Louie <joseph161216@gmail.com>
2019-10-23 00:30:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,9 +5,8 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
-  nds
 end
 
 def print_first_directors_movie_titles
-  pretty_print_nds
+  nds
 end
