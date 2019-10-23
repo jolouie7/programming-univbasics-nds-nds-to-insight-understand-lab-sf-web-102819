7d255574593cd4f0954ec08e31f4b499e4042e24
Joseph  Louie <joseph161216@gmail.com>
2019-10-23 00:40:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,10 +9,10 @@ end
 
 def print_first_directors_movie_titles
   db = directors_database
-  # i = 0 
-  # while i < db.length do 
-  #   puts db[0]
-  #   i += 1
-  # end 
-  puts db[0][:movies][0][:title]
+  i = 0 
+  while i < db.length do 
+    puts db[0]
+    i += 1
+  end 
+  # puts db[0][:movies][0][:title]
 end
